0049031071f2147495fc9287b6c87ec1539c302c
sergiogrdzestudio <sergiogrdzestudio@outlook.com>
2020-08-05 01:05:06 -0500

    docs agregado

---
+++ docs/precache-manifest.fba9ef3a68adf529b50b1a567d33f407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46d1d716d36857a501fd078c95386ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b5976c9cfa281b99537",
+    "url": "/static/css/main.7466f302.chunk.css"
+  },
+  {
+    "revision": "2413f55b153c85fde29e",
+    "url": "/static/js/2.be58aea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.be58aea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b5976c9cfa281b99537",
+    "url": "/static/js/main.6de385f7.chunk.js"
+  },
+  {
+    "revision": "722a304d2cb62627ceb8",
+    "url": "/static/js/runtime-main.3cc8bee4.js"
+  }
+]);
